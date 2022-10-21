305e7dbe9526a4f1cf8186f698f69cacf7447273
gil037 <dorayaki.lawersclub@gmail.com>
2022-10-22 03:16:06 +0800

    terbaru

--- Submission Fundamental Android/submission 2/app/src/main/java/com/example/github2/data/remote/response/ResponseUser.kt
+++
@@ -1,8 +0,0 @@
-package com.example.github2.data.remote.response
-
-import com.google.gson.annotations.SerializedName
-
-data class ResponseUser(
-    @field:SerializedName("items")
-    val items: List<User>
-)
